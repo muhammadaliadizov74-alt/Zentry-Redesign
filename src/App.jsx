a17e6e26e8573d0a4a094ba67f965bac3fdeea1a
Favour Alfred <alfredfavour76@gmail.com>
2025-08-18 11:18:36 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Navbar />
-      <Hero />
+      {/*<Hero />*/}
       <About />
       <Features />
       <Story />
